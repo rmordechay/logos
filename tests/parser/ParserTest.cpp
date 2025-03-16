aed53a7a8d51228ed05c92806a53f1aa9adb8172
Roi Mordechay <73055476+rmordechay@users.noreply.github.com>
2025-03-16 12:36:38 +0100

    Updated tests
    Former-commit-id: b8c0818cfbcaaa3b74bb3d222ce08b4666153150
    Former-commit-id: 58d724253ce320109fe887e3f6bc6ec26fefea03
    Former-commit-id: 3c7f66d12e9efe536be95a437952f998a9cacbff

--- tests/parser/ParserTest.cpp
+++ tests/parser/ParserTest.cpp
@@ -2,20 +2,16 @@
 #include <gtest/gtest.h>
 #include <LogosParser.h>
 
-TEST(ParserTest, TestParsingWorks) {
-    const Logos project("data");
+
+TEST(ParserTest, TestParseFile) {
+    const Logos project("../tests/parser/data");
     project.initPaths();
-    const directory_entry fileEntry("../tests/data/Main.lgs");
+    const directory_entry fileEntry(rootDir / "Main.lgs");
     const auto file = project.parseFile(fileEntry);
     const auto mainFile = dynamic_cast<LgsMainFile*>(file);
     EXPECT_NE(mainFile, nullptr);
     ASSERT_EQ(mainFile->name, "Main");
     EXPECT_NE(mainFile->relPath, "");
     EXPECT_NE(mainFile->mainFunc, nullptr);
-    EXPECT_GT(mainFile->funcs.size(), 0);
-}
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    EXPECT_EQ(mainFile->funcs.size(), 2);
 }
