e9bdd5cc79ab9a1e733f8a82fb5ec9c6e3a15189
Roi Mordechay <73055476+rmordechay@users.noreply.github.com>
2025-03-25 01:49:54 +0100

    Added enum tests
    Former-commit-id: 8825919c841650b5fdc63a6bcf9fb3c0c02fd7f2
    Former-commit-id: c45b0923be2e31ac54d42498702ce68f931766ee
    Former-commit-id: 3ab26901b066ce6581288589568c6112e3486ffb

---
+++ tests/analysis/sema/enums/SemaEnumsTests.cpp
@@ -0,0 +1,38 @@
+#include "LgsLinker.h"
+#include "Logos.h"
+#include "TestUtils.h"
+
+#include <filesystem>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using testing::StartsWith;
+
+class SemaEnumsTests : public testing::Test {
+protected:
+    path dataDir = "../tests/analysis/sema/enums/";
+    Logos logos = Logos(dataDir);
+
+    void SetUp() override {
+        logos.loadBuiltinFuncs();
+    }
+
+    void TearDown() override {
+        remove_all(logos.buildDir);
+    }
+};
+
+TEST_F(SemaEnumsTests, TestEnumsWorks) {
+    const auto code = R"(
+    enum MyEnum {
+        A = "value A"
+        B = "value A"
+    }
+
+    main() {
+
+    }
+    )";
+    const auto file = logos.parseFile(code);
+    logos.analyse({file});
+}
