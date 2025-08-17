ee0cba257fae66f3219d8132c12b2448b3ac642c
Roi Mordechay <73055476+rmordechay@users.noreply.github.com>
2025-08-17 17:27:09 +0200

    Updated map to c++ map

--- runtime/lib/Lgs_hashmap.cpp
+++ runtime/lib/Lgs_hashmap.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <vector>
 #include <cstring>
-#include <cstdlib>
 
 struct Lgs_Map {
     size_t valueSize;
@@ -15,23 +14,22 @@ extern "C" void Lgs_Map_init(Lgs_Map* map, const size_t valueSize) {
     map->data = new std::unordered_map<std::string, std::vector<char>>();
 }
 
-extern "C" void Lgs_Map_add(Lgs_Map* map, const char* key, const void* value) {
+extern "C" void Lgs_Map_add(const Lgs_Map* map, const char* key, const void* value) {
     if (!map || !key || !value) std::exit(1);
     if (std::strlen(key) > 1024) std::exit(1);
-
     auto& vec = (*map->data)[key];
     vec.resize(map->valueSize);
     std::memcpy(vec.data(), value, map->valueSize);
 }
 
-extern "C" void* Lgs_Map_get(Lgs_Map* map, const char* key) {
+extern "C" void* Lgs_Map_get(const Lgs_Map* map, const char* key) {
     if (!map || !key) std::exit(1);
     auto it = map->data->find(key);
     if (it == map->data->end()) return nullptr;
     return it->second.data();
 }
 
-extern "C" void Lgs_Map_delete(Lgs_Map* map, const char* key) {
+extern "C" void Lgs_Map_delete(const Lgs_Map* map, const char* key) {
     if (!map || !key) std::exit(1);
     map->data->erase(key);
 }
@@ -48,7 +46,7 @@ extern "C" bool Lgs_Map_isNotEmpty(const Lgs_Map* map) {
     return map ? !map->data->empty() : false;
 }
 
-extern "C" void Lgs_Map_free(Lgs_Map* map) {
+extern "C" void Lgs_Map_free(const Lgs_Map* map) {
     if (!map) return;
     delete map->data;
 }
