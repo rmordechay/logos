25491dd9e52cf5c4f2ebe1ed6214fd3dbb30157f
Roi Mordechay <73055476+rmordechay@users.noreply.github.com>
2025-10-18 18:49:51 +0200

    Removed scheduler

--- runtime/lib/Lgs_runtime.cpp
+++ runtime/lib/Lgs_runtime.cpp
@@ -1,27 +1,19 @@
-#include "Lgs_scheduler.h"
 #include "Lgs_stack.h"
 #include "Lgs_types.h"
-#include <sqlite3.h>
 
 struct Lgs_darray;
 
 struct Lgs_runtime {
     Lgs_stack stack;
-    Lgs_scheduler scheduler;
     std::map<void*, std::map<std::string, void*>> vtable;
-    sqlite3* db;
 };
 
 static inline Lgs_runtime runtime;
 
 extern "C" void Lgs_runtime_init() {
-    // runtime.scheduler.start();
-    //sqlite3_open(":memory:", &runtime.db);
 }
 
 extern "C" void Lgs_runtime_close() {
-    // runtime.scheduler.shutdown();
-    // sqlite3_close(runtime.db);
 }
 
 extern "C" void Lgs_stack_addOwner(void* ptr, const Lgs_rttype type) {
@@ -48,29 +40,8 @@ extern "C" void Lgs_stack_addDefer(void* funcPtr, void* ctx) {
     runtime.stack.addDefer(funcPtr, ctx);
 }
 
-extern "C" void Lgs_stack_addCoro(void* funcPtr, void* ctx) {
-    const auto thunk = reinterpret_cast<Lgs_Defer_Func>(funcPtr);
-    runtime.scheduler.spawn(thunk, ctx);
-}
-
 extern "C" void Lgs_stack_callDefers() {
     runtime.stack.callDefers();
-}
-
-extern "C" void Lgs_scheduler_spawn(void (*task)(void*), void* ctx) {
-    runtime.scheduler.spawn(task, ctx);
-}
-
-extern "C" void Lgs_scheduler_yield() {
-    runtime.scheduler.yield();
-}
-
-extern "C" bool Lgs_scheduler_shouldYield() {
-    return runtime.scheduler.shouldYield();
-}
-
-extern "C" void Lgs_scheduler_shutdown() {
-    runtime.scheduler.shutdown();
 }
 
 extern "C" void Lgs_vtable_add(void* instancePtr, const char* name, void* ptr) {
